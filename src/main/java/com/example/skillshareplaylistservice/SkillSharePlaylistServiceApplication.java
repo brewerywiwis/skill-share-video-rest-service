22ff34367adfeb7bc49be8b842d7ab96adeffb1a
brewerywiwis <brewder136@hotmail.com>
2021-11-12 21:22:11 +0700

    change project name

--- src/main/java/com/example/skillshareplaylistservice/SkillSharePlaylistServiceApplication.java
+++ src/main/java/com/example/skillshareplaylistservice/SkillSharePlaylistServiceApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@OpenAPIDefinition(info = @Info(title = "Skill Share: Playlist service API", version = "1.0"))
+@OpenAPIDefinition(info = @Info(title = "Skill Share: Video rest service API", version = "1.0"))
 public class SkillSharePlaylistServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(SkillSharePlaylistServiceApplication.class, args);
---
+++ src/main/java/com/example/skillshareplaylistservice/repository/CommentRepository.java
@@ -0,0 +1,4 @@
+package com.example.skillshareplaylistservice.repository;
+
+public class CommentRepository {
+}
